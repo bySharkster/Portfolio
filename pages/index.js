bb497f2617b67844b4e3088184d90d939d840ea9
bySharkster <fernandoaponte0609@gmail.com>
2023-01-19 20:29:36 -0400

    update on typos

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             <h1 className="mb-5 font-serif text-5xl font-bold">Fernando Aponte</h1>
-            <p className="mb-5 font-serif text-justify indent-4 " >Fernando Aponte, an automation engineer with over a year of experience, has a proven track record of designing and implementing innovative automation solutions to improve efficiency and productivity. He has a strong background in programming languages such as Javascript, Python, VBA and query lenguages like SQL. Has experience in various automation tools like Batch, VMWare, and PLC. With a passion for technology and problem-solving, Fernando is dedicated to driving results and staying ahead of the curve in the rapidly evolving field of automation engineering.</p>
+            <p className="mb-5 font-serif text-justify indent-4 " >Fernando Aponte, an Automation Engineer with over a year of experience, has a proven track record of designing and implementing innovative automation solutions to improve efficiency and productivity. He has a strong background in programming languages such as Javascript, Python, VBA and query lenguages like SQL. Has experience in various automation tools like Batch, VMWare, and PLC. With a passion for technology and problem-solving, Fernando is dedicated to driving results and staying ahead of the curve in the rapidly evolving field of automation engineering.</p>
             <Link role="button" href="/portfolio" className="btn btn-info disabled:opacity-75 hover:scale-125 hover:shadow-xl hover:font-bold hover:text-white" disabled>My Projects</Link>
           </div>
         </div>
