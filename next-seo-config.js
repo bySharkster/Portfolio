9d70b409a1c32c71331307a9526cde94cd1d3eb7
bySharkster <fernandoaponte0609@gmail.com>
2023-02-01 22:26:36 -0400

    SEO update

---
+++ next-seo-config.js
@@ -0,0 +1,9 @@
+export default {
+    openGraph: {
+      type: "website",
+      url: "https://www.fernandoaponte.dev/",
+      title: "Fernando Aponte",
+      description: "This is Fernando Aponte's online portfolio",
+    },
+
+  };
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
 import '../styles/custom_ss.css'
 import { Layout } from '../components/Layout/Layout'
+import { DefaultSeo } from 'next-seo';
+import SEO from "../next-seo.config";
 
 export default function App({ Component, pageProps }) {
-  return (<Layout><Component {...pageProps} /></Layout>)
+  return (<Layout><DefaultSeo{...SEO}/><Component {...pageProps} /></Layout>)
 }
