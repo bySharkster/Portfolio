75ebcd0e882916daf2c3e2ac9757a1a6ce3338d1
bySharkster <fernandoaponte0609@gmail.com>
2023-02-01 22:28:00 -0400

    SEO Update

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import '../styles/custom_ss.css'
 import { Layout } from '../components/Layout/Layout'
 import { DefaultSeo } from 'next-seo';
-import SEO from "../next-seo.config";
+import SEO from "../next-seo-config.js";
 
 export default function App({ Component, pageProps }) {
   return (<Layout><DefaultSeo{...SEO}/><Component {...pageProps} /></Layout>)
