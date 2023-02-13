bee8c049d0fae46a296a714deca124950d8c449b
bySharkster <fernandoaponte0609@gmail.com>
2023-02-12 20:10:44 -0400

    img commita

--- pages/contact.js
+++ pages/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     return (
       <div className="hero min-h-screen" style={{ backgroundImage: `url("https://i.ibb.co/hHpVwKQ/desktop-1920x1080.png")` }}>
         <div className="flex mx-auto my-auto mt-4 card  w-96 bg-base-100 shadow-md  hover:shadow-xl scale-95 hover:scale-100">
-        <figure><img src="https://i.ibb.co/61mPgWz/IMG-20230115-WA0168.jpg" alt="MeIcon" /></figure>
+        <figure><img src="contact.jpg" alt="MeIcon" /></figure>
         <div className="card-body card-glow ">
           <h2 className="card-title">Contact Me!</h2>
           <div className="flex items-center">
