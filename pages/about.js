082f3fa16bbe8d432290084f8a0cf06c8ea0661b
bySharkster <fernandoaponte0609@gmail.com>
2023-02-12 20:34:39 -0400

    Resume Update

--- pages/about.js
+++ pages/about.js
@@ -8,57 +8,74 @@ export default function About() {
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="bg-white p-6 rounded-lg shadow-md">
                 <div className="text-2xl font-medium">Fernando J. Aponte-Aponte</div>
-                <div className="mt-4 flex items-center">
-                    <FaPhone className="text-gray-600 mr-2" />
-                    <div className="text-gray-700">Personal: (939) 280 - 8824</div>
-                </div>
-                <div className="mt-2 flex items-center">
-                    <FaEnvelope className="text-gray-600 mr-2" />
-                    <a href="mailto:fernandoaponte0609@gmail.com" className="text-gray-700">fernandoaponte0609@gmail.com</a>
+                <div className="flex items-center mt-10">
+                        <FaPhone className="text-gray-600 mr-2 ml-4" />
+                        <div className="text-gray-700">Personal: (939) 280 - 8824</div>
+                        <FaEnvelope className="text-gray-600 mr-2 ml-4" />
+                        <a href="mailto:fernandoaponte0609@gmail.com" className="text-gray-700">fernandoaponte0609@gmail.com</a>
                 </div>
                 <div className="mt-6">
                     <div className="text-lg font-medium">Objective</div>
                     <hr/>
-                    <div className="text-gray-700">I’m searching for a company where I can demonstrate my skills and knowledge while contributing to the success of the team.</div>
-                </div>
-                <div className="mt-6">
-                    <div className="text-lg font-medium">Education</div>
-                    <hr/>
-                    <div className="text-gray-700">Interamerican University of Puerto Rico, San German Campus <br/> <strong>Bachelor of Computer Science</strong> <br/> Graduated in 2022</div>
-                </div>
-                <div className="mt-6">
-                    <div className="text-lg font-medium">Computer Skills</div>
-                    <hr/>
-                    <div className="text-gray-700">
-                    <ul className="list-disc list-inside ">
-                        <li>Logical Programming</li>
-                        <li>Next.JS, React</li>
-                        <li>Web page development (HTML, JavaScript,PHP)</li>
-
-                        <li>VMWare & PLC</li>
-                        <li>Databases (SQL)</li>
-                    </ul>
-
+                    <div className="text-gray-700 text-center">Automation Engineer with over a year of experience with a proven track record <br/>of designing and implementing innovative automation solutions to improve efficiency and productivity. Can work with
+                    <br/>programming languages such as Javascript, Python, VBA and query languages like SQL. Also can work
+                    <br/>with automation tools like Batch, VMWare, and PLC. With a passion for technology and problemsolving dedicated to <br/> driving results and staying ahead of the curve in the rapidly evolving field of automation engineering.</div>
                     </div>
-                </div>
-                <div className="mt-6">
-                    <div className="text-lg font-medium">Experiences</div>
-                    <hr/>
-                    <div className="text-gray-700">
-                        <h3 className="text-gray-900 text-center decoration-success-content font-serif underline">JC Automation</h3>
-                        <ul className="list-disc list-inside ">Automation Engineer
-                        <li>Troubloshooting and configuration of PLC, Microsoft SQL, VMware, FactoryTalk and batch related issues on field.</li>
-                        <li>Documentation Updates and Creation as per Automation viewpoint.</li>
-                        <li>Support during validations of New System/Equipments.</li>
+                    <div className="mt-6">
+                        <div className="text-lg font-medium">Academics</div>
+                        <hr/>
+                        <div className="text-gray-700">Interamerican University of Puerto Rico, San German Campus <br/> <strong>Bachelor of Computer Science</strong> <br/> </div>
+                    </div>
+                    <div className="mt-6">
+                        <div className="text-lg font-medium">Key Skills</div>
+                        <hr/>
+                        <div className="text-gray-700">
+                        <ul className="list-disc list-inside ">
+                            <li>Logical Programming</li>
+                            <li>Next.JS, React</li>
+                            <li>Web page development (HTML, JavaScript,PHP)</li>
+                            <li>VMWare & PLC</li>
+                            <li>Databases (SQL)</li>
                         </ul>
+
+                        </div>
                     </div>
-                </div>
-                <div className="mt-6 flex justify-center items-center">
-                    <br/>
-                    <div className="flex"><Link type="button"  href="/coming_soon" className="btn-ast sm:btn-ast-sm md:btn-ast-md lg:btn-ast-lg sm:mr-0 md:mr-2 lg:mr-4"><span>My Projects</span></Link></div>
-                    <div className="flex"><DownloadLink href="/Fernando_Aponte_Resume.pdf" fileName="Fernando_Aponte_Resume.pdf"></DownloadLink></div>
+                    <div className="mt-6">
+                        <div className="text-lg font-medium">Experience</div>
+                        <hr/>
+                        <div className="text-gray-700">
+                            <p>Present</p>
+                            <h3 className="text-gray-900 text-left decoration-success-content font-serif underline">JC Automation</h3>
+                            <ul className="list-disc list-inside ">Automation Engineer
+                            <li>Troubloshooting and configuration of PLC, Microsoft SQL, VMware, FactoryTalk and batch related issues on field.</li>
+                            <li>Documentation Updates and Creation as per Automation viewpoint.</li>
+                            <li>Support during validations of New System/Equipments.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="mt-6">
+                        <div className="text-lg font-medium">Abilities</div>
+                        <hr/>
+                        <div className="text-gray-700">
+                            
+                            <ul className="list-disc list-inside ">
+                            <li>Bilingiual: Spanish / English</li>
+                            <li>Team Leader</li>
+                            <li>Responsible</li>
+                            <li>Fast Learner</li>
+                            <li>Complex Problem Solving</li>
+                            <li>Achieve goals in record time</li>
+                            <li>Critcal Thinking</li>
+                            <li>Operations Analysis</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="mt-6 flex justify-center items-center">
+                        <br/>
+                        <div className="flex"><Link type="button"  href="/coming_soon" className="btn-ast sm:btn-ast-sm md:btn-ast-md lg:btn-ast-lg sm:mr-0 md:mr-2 lg:mr-4"><span>My Projects</span></Link></div>
+                        <div className="flex"><DownloadLink href="/Fernando_Aponte_Resume.pdf" fileName="Fernando_Aponte_Resume.pdf"></DownloadLink></div>
+                    </div>
                 </div>
             </div>
-        </div>
         )
 }
